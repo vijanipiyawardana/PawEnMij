395b3d12f111738ab7c14357c4d7fcb1bdad689c
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-07-05 18:13:30 +0200

    Setting up boilerplate code

---
+++ backend/src/main/java/com/vijani/pawenmij/PawenmijApplication.java
@@ -0,0 +1,13 @@
+package com.vijani.pawenmij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawenmijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawenmijApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vijani/pawenmij/PawenmijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijani.pawenmij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawenmijApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
