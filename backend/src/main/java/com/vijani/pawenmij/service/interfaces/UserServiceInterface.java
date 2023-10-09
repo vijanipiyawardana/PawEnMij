190e6bb642d6923393f7f5915b5453b74e1d046f
Vijani <vijanipiyawardana@gmail.com>
2023-10-09 15:54:43 +0200

    added interface for user service

---
+++ backend/src/main/java/com/vijani/pawenmij/service/interfaces/UserServiceInterface.java
@@ -0,0 +1,7 @@
+package com.vijani.pawenmij.service.interfaces;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserServiceInterface {
+    UserDetailsService userDetailsService();
+}
