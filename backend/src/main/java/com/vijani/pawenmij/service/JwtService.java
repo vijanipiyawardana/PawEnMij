f392eb7bd681afeb68fcf833a08aca317b51070d
Vijani <vijanipiyawardana@gmail.com>
2023-10-11 20:40:42 +0200

    Added @Service annotation

--- backend/src/main/java/com/vijani/pawenmij/service/JwtService.java
+++ backend/src/main/java/com/vijani/pawenmij/service/JwtService.java
@@ -9,6 +9,7 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
@@ -16,6 +17,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+@Service
 public class JwtService implements JwtServiceInterface {
 
     @Value("${token.signing.key}")
