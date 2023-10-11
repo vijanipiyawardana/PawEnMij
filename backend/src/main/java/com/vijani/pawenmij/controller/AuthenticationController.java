3ecade25a6df9f5d0bff66a44c4bea0ce15f1686
Vijani <vijanipiyawardana@gmail.com>
2023-10-11 20:37:37 +0200

    Added controller classes for Authenticattion and Autherization

---
+++ backend/src/main/java/com/vijani/pawenmij/controller/AuthenticationController.java
@@ -0,0 +1,29 @@
+package com.vijani.pawenmij.controller;
+
+import com.vijani.pawenmij.dto.request.SignUpRequest;
+import com.vijani.pawenmij.dto.request.SigninRequest;
+import com.vijani.pawenmij.dto.response.JwtAuthenticationResponse;
+import com.vijani.pawenmij.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+    @PostMapping("/signup")
+    public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest request) {
+        return ResponseEntity.ok(authenticationService.signup(request));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SigninRequest request) {
+        return ResponseEntity.ok(authenticationService.signin(request));
+    }
+}
